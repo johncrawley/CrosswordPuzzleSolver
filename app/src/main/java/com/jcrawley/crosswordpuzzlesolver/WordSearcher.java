9887f1ea141490e4ce62064d4fcc934064eb79c8
johncrawley <johncrawley1@gmail.com>
2021-05-12 22:41:21 +0100

    Removed words with incorrect length, or apostrophes from the results found. Also sorted results.

--- app/src/main/java/com/jcrawley/crosswordpuzzlesolver/WordSearcher.java
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/WordSearcher.java
@@ -1,26 +1,35 @@
 package com.jcrawley.crosswordpuzzlesolver;
 
 import java.util.ArrayList;
-import java.util.HashMap;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Set;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class WordSearcher {
 
     private String words;
-
+    private List<String> badCharacters;
+    private HashSet<String> foundWords;
+    private int expectedCharacters;
 
     public WordSearcher(String words){
+
         this.words = words;
+        setupBadCharacters();
+        foundWords = new HashSet<>();
+    }
+
+    private void setupBadCharacters(){
+        badCharacters = Arrays.asList(" ", "'");
     }
 
 
     List<String> searchFor(String searchText){
         final String WORD_SEPARATOR = "\\b";
-
+        expectedCharacters = searchText.length();
         String inputText = WORD_SEPARATOR + searchText + WORD_SEPARATOR;
         Pattern pattern = Pattern.compile(inputText);
         return getMatchingWords(pattern);
@@ -28,17 +37,43 @@ public class WordSearcher {
 
 
     private List<String> getMatchingWords(Pattern tagMatcher) {
-        Set<String> foundWords = new HashSet<>();
+        foundWords.clear();
         Matcher matcher = tagMatcher.matcher(this.words);
         while(matcher.find()) {
-            String result = matcher.group();
-            result = result.trim();
-            if(!result.contains(" ")){
-                foundWords.add(result);
+            processResult(matcher.group());
+        }
+        List<String> results = new ArrayList<>(foundWords);
+        Collections.sort(results);
+        return results;
+    }
+
+
+    private void processResult(String result){
+        String word = result.trim();
+        if(hasValidCharactersOnly(word) && isCorrectLength(word)){
+            foundWords.add(word);
+        }
+    }
+
+
+    private boolean hasValidCharactersOnly(String word){
+        for(String badCharacter:  badCharacters){
+            if(word.contains(badCharacter)){
+                return false;
             }
         }
-        return new ArrayList<>(foundWords);
+        return true;
     }
+
+
+    private void log(String msg){
+        System.out.println("WordSearcher: " + msg);
+    }
+
+    private boolean isCorrectLength(String word){
+        return word.length() == expectedCharacters;
+    }
+
 
 
 }
