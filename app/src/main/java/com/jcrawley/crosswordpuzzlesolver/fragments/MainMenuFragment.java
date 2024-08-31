d4859fb14105d268c4d31ac868a23ba39a274d40
john <johncrawley1@gmail.com>
2024-08-31 23:59:18 +0100

    Added fragment utils and animations.

--- app/src/main/java/com/jcrawley/crosswordpuzzlesolver/fragments/MainMenuFragment.java
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/fragments/MainMenuFragment.java
@@ -1,46 +1,16 @@
 package com.jcrawley.crosswordpuzzlesolver.fragments;
 
-import android.content.Context;
 import android.os.Bundle;
-import android.os.Handler;
-import android.os.Looper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.view.inputmethod.EditorInfo;
-import android.view.inputmethod.InputMethodManager;
-import android.widget.ArrayAdapter;
-import android.widget.EditText;
-import android.widget.ListView;
-import android.widget.TextView;
+import android.widget.Button;
 
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.ViewModelProvider;
-
 import com.jcrawley.crosswordpuzzlesolver.R;
-import com.jcrawley.crosswordpuzzlesolver.anagram.AnagramFinder;
-import com.jcrawley.crosswordpuzzlesolver.viewModel.MainViewModel;
-
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.concurrent.Executor;
-import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
+import com.jcrawley.crosswordpuzzlesolver.fragments.utils.FragmentUtils;
 
 public class MainMenuFragment extends Fragment {
-
-    private EditText lettersEditText, requiredLettersEditText;
-    private TextView resultsCountTextView, noResultsFoundTextView;
-    private View listDivider;
-    private Context context;
-    private String previousSearch;
-    private ArrayAdapter<String> arrayAdapter;
-    private List<String> results;
-    private AnagramFinder anagramFinder;
-    private Executor findWordsExecutor;
-
 
     public MainMenuFragment() {
         // Required empty public constructor
@@ -50,23 +20,22 @@ public class MainMenuFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        context = getContext();
-        View parentView = inflater.inflate(R.layout.find_words, container, false);
-        MainViewModel viewModel = new ViewModelProvider(requireActivity()).get(MainViewModel.class);
-
-        results = new ArrayList<>();
+        View parentView = inflater.inflate(R.layout.main_menu, container, false);
         setupViews(parentView);
         return parentView;
     }
 
 
     private void setupViews(View parentView){
-        lettersEditText = parentView.findViewById(R.id.lettersInputEditText);
-        requiredLettersEditText = parentView.findViewById(R.id.requiredLettersInputEditText);
-        listDivider = parentView.findViewById(R.id.listDivider);
-        resultsCountTextView = parentView.findViewById(R.id.resultsCountTextView);
+        setupButton(parentView, R.id.findWordsButton, ()-> FragmentUtils.loadFindWords(this));
+        setupButton(parentView, R.id.wordCheckerButton, ()-> FragmentUtils.loadCheckWord(this));
+        setupButton(parentView, R.id.patternWordsButton, ()-> FragmentUtils.loadRegex(this));
     }
 
+    private void setupButton(View parentView, int buttonId, Runnable action){
+        Button button = parentView.findViewById(buttonId);
+        button.setOnClickListener( v-> action.run());
+    }
 
 
 }
---
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/fragments/utils/FragmentUtils.java
@@ -0,0 +1,125 @@
+package com.jcrawley.crosswordpuzzlesolver.fragments.utils;
+
+import android.os.Bundle;
+
+import androidx.activity.OnBackPressedCallback;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.jcrawley.crosswordpuzzlesolver.fragments.FindWordsFragment;
+import com.jcrawley.crosswordpuzzlesolver.fragments.PuzzleHelperFragment;
+import com.jcrawley.crosswordpuzzlesolver.fragments.RegexFragment;
+import com.jcrawley.crosswordpuzzlesolver.fragments.WordExistsFragment;
+import com.jcrawley.crosswordpuzzlesolver.R;
+
+import java.util.function.Consumer;
+
+public class FragmentUtils {
+
+
+    public static void showDialog(Fragment parentFragment, DialogFragment dialogFragment, String tag, Bundle bundle){
+        FragmentManager fragmentManager = parentFragment.getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        removePreviousFragmentTransaction(fragmentManager, tag, fragmentTransaction);
+        dialogFragment.setArguments(bundle);
+        dialogFragment.show(fragmentTransaction, tag);
+    }
+
+
+    public static void loadFindWords(Fragment parentFragment){
+        loadFragment(parentFragment, new FindWordsFragment(), "find_words");
+    }
+
+
+    public static void loadPuzzleHelper(Fragment parentFragment){
+        loadFragment(parentFragment, new PuzzleHelperFragment(), "puzzle_helper");
+    }
+
+
+    public static void loadRegex(Fragment parentFragment){
+        loadFragment(parentFragment, new RegexFragment(), "regex");
+    }
+
+
+    public static void loadCheckWord(Fragment parentFragment){
+        loadFragment(parentFragment, new WordExistsFragment(), "check_word");
+    }
+
+
+    public static void loadFragmentOnBackButtonPressed(Fragment parentFragment, Fragment destinationFragment, String fragmentTag){
+        onBackButtonPressed(parentFragment, () -> loadFragment(parentFragment, destinationFragment, fragmentTag));
+    }
+
+
+    public static void loadFragment(Fragment parentFragment, Fragment fragment, String tag, Bundle bundle){
+        FragmentManager fragmentManager = parentFragment.getParentFragmentManager();
+        FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
+        removePreviousFragmentTransaction(fragmentManager, tag, fragmentTransaction);
+        fragment.setArguments(bundle);
+        fragmentManager.beginTransaction()
+                .setCustomAnimations(R.anim.slide_in, R.anim.slide_out, R.anim.pop_enter, R.anim.pop_exit )
+                .replace(R.id.fragment_container, fragment, tag)
+                .addToBackStack(null)
+                .commit();
+    }
+
+
+    public static void onBackButtonPressed(Fragment parentFragment, Runnable action){
+        OnBackPressedCallback callback = new OnBackPressedCallback(true) {
+            @Override
+            public void handleOnBackPressed() {
+                action.run();
+            }
+        };
+        parentFragment.requireActivity().getOnBackPressedDispatcher().addCallback(parentFragment.getViewLifecycleOwner(), callback);
+    }
+
+
+    public static void loadFragment(Fragment parentFragment, Fragment fragment, String tag){
+        loadFragment(parentFragment, fragment, tag, new Bundle());
+    }
+
+
+    private static void removePreviousFragmentTransaction(FragmentManager fragmentManager, String tag, FragmentTransaction fragmentTransaction){
+        Fragment prev = fragmentManager.findFragmentByTag(tag);
+        if (prev != null) {
+            fragmentTransaction.remove(prev);
+        }
+        fragmentTransaction.addToBackStack(null);
+    }
+
+
+    public static void setListener(Fragment fragment, String key, Consumer<Bundle> consumer){
+        fragment.getParentFragmentManager().setFragmentResultListener(key, fragment, (requestKey, bundle) -> consumer.accept(bundle));
+    }
+
+
+    public static void sendMessage(Fragment fragment, String key){
+        sendMessage(fragment, key, new Bundle());
+    }
+
+
+    public static void sendMessage(Fragment fragment, String key, Bundle bundle){
+        fragment.getParentFragmentManager().setFragmentResult(key, bundle);
+    }
+
+
+    public static int getInt(Bundle bundle, Enum<?> tag){
+        return bundle.getInt(tag.name());
+    }
+
+
+    public static String getStr(Bundle bundle, Enum<?> tag){
+        return bundle.getString(tag.name());
+    }
+
+
+    public static boolean getBoolean(Bundle bundle, Enum<?> tag){
+        return  bundle.getBoolean(tag.name());
+    }
+
+
+}
