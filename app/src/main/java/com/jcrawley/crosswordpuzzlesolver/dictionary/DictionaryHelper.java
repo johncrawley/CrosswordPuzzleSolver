da3081796bc1d5a738191e1307b0d70d7afa9a83
john <johncrawley1@gmail.com>
2026-01-08 23:58:52 +0000

    Created a DictionaryHelper and DictionaryHelperImpl to take over work from the dictionary service.

---
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/dictionary/DictionaryHelper.java
@@ -0,0 +1,14 @@
+package com.jcrawley.crosswordpuzzlesolver.dictionary;
+
+import com.jcrawley.crosswordpuzzlesolver.WordListView;
+
+import java.util.List;
+
+public interface DictionaryHelper {
+
+    void setWords(List<String> words);
+    void runPuzzleHelperSearch(String inputText, String excludedLettersStr, boolean isUsingAnagrams, WordListView wordListView);
+    void getResultsForPattern(String pattern, WordListView wordListView);
+    void findWords(String input, String requiredLetters, WordListView wordListView);
+    boolean doesWordExist(String word);
+}
---
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/dictionary/DictionaryHelperImpl.java
@@ -0,0 +1,32 @@
+package com.jcrawley.crosswordpuzzlesolver.dictionary;
+
+import com.jcrawley.crosswordpuzzlesolver.WordListView;
+
+import java.util.List;
+
+public class DictionaryHelperImpl implements DictionaryHelper{
+    @Override
+    public void setWords(List<String> words) {
+
+    }
+
+    @Override
+    public void runPuzzleHelperSearch(String inputText, String excludedLettersStr, boolean isUsingAnagrams, WordListView wordListView) {
+
+    }
+
+    @Override
+    public void getResultsForPattern(String pattern, WordListView wordListView) {
+
+    }
+
+    @Override
+    public void findWords(String input, String requiredLetters, WordListView wordListView) {
+
+    }
+
+    @Override
+    public boolean doesWordExist(String word) {
+        return false;
+    }
+}
