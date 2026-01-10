584013720592507b53ef478a2730e0d8d4b76293
john <johncrawley1@gmail.com>
2026-01-10 22:22:16 +0000

    Building out Dictionary Helper class before removal of service.

--- app/src/main/java/com/jcrawley/crosswordpuzzlesolver/dictionary/DictionaryHelperImpl.java
+++ app/src/main/java/com/jcrawley/crosswordpuzzlesolver/dictionary/DictionaryHelperImpl.java
@@ -4,10 +4,17 @@ import com.jcrawley.crosswordpuzzlesolver.WordListView;
 import com.jcrawley.crosswordpuzzlesolver.WordSearcher;
 import com.jcrawley.crosswordpuzzlesolver.anagram.AnagramFinder;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.stream.Collectors;
 
 public class DictionaryHelperImpl implements DictionaryHelper{
 
@@ -16,6 +23,13 @@ public class DictionaryHelperImpl implements DictionaryHelper{
     private final AtomicBoolean isSearchRunning = new AtomicBoolean(false);
     private final ExecutorService executor = Executors.newSingleThreadExecutor();
     private final AtomicBoolean isDictionaryLoaded = new AtomicBoolean(false);
+    private CountDownLatch dictionaryLatch;
+    private Map<String, Set<String>> wordsMap;
+
+
+    public DictionaryHelperImpl(){
+        dictionaryLatch = new CountDownLatch(1);
+    }
 
 
     @Override
@@ -27,10 +41,13 @@ public class DictionaryHelperImpl implements DictionaryHelper{
     @Override
     public void init(Dictionary dictionary){
         isDictionaryLoaded.set(false);
+        dictionaryLatch = new CountDownLatch(1);
+        wordsMap = dictionary.wordsMap();
         wordSearcher = new WordSearcher(dictionary.wordsList(), dictionary.wordsStr());
-        anagramFinder.setupWordsMap(dictionary.wordsMap());
+        anagramFinder.setupWordsMap(wordsMap);
         anagramFinder.setWordsByLengthMap(dictionary.wordsByLengthMap());
         isDictionaryLoaded.set(true);
+        dictionaryLatch.countDown();
     }
 
 
@@ -39,23 +56,119 @@ public class DictionaryHelperImpl implements DictionaryHelper{
 
     }
 
+
     @Override
     public void runPuzzleHelperSearch(String inputText, String excludedLettersStr, boolean isUsingAnagrams, WordListView wordListView) {
-
+        waitForDictionaryToLoad();
+        ifNotSearching(()->{
+            String formattedInput = inputText.trim().toLowerCase();
+            if (formattedInput.isEmpty()) {
+                wordListView.setWords(Collections.emptyList());
+            }
+            var initialResults = getInitialResultsFor(formattedInput, isUsingAnagrams);
+            List<String> results = new ArrayList<>(excludeWordsWithDisallowedLetters(initialResults, excludedLettersStr));
+            wordListView.setWords(results);
+        });
     }
+
 
     @Override
     public void getResultsForPattern(String pattern, WordListView wordListView) {
-
+        waitForDictionaryToLoad();
+        ifNotSearching(()->{
+            List<String> results = wordSearcher.searchForPattern(pattern);
+            wordListView.setWords(results);
+        });
     }
+
 
     @Override
     public void findWords(String input, String requiredLetters, WordListView wordListView) {
-
+        waitForDictionaryToLoad();
+        ifNotSearching(()-> wordListView.setWords(anagramFinder.getWordsFrom(input, requiredLetters)));
     }
+
 
     @Override
     public boolean doesWordExist(String word) {
+        waitForDictionaryToLoad();
+        String sortedWord = getSortedWord(word);
+        if(wordsMap.containsKey(sortedWord)){
+            Set<String> words = wordsMap.get(sortedWord);
+            return words != null && words.contains(word);
+        }
         return false;
     }
+
+
+    private List<String> excludeWordsWithDisallowedLetters(List<String> initialResults, String excludedLettersStr){
+        if(excludedLettersStr.isEmpty()){
+            return new ArrayList<>(initialResults);
+        }
+        List<String> excludedLetters = Arrays.asList(excludedLettersStr.split(""));
+        return createListOfAllowedWords(initialResults, excludedLetters);
+    }
+
+
+    private List<String> createListOfAllowedWords(List<String> inputList, List<String> excludedLetters){
+        return inputList.stream()
+                .filter(word -> isWordFreeOfExcludedLetters(word, excludedLetters))
+                .collect(Collectors.toList());
+    }
+
+
+    private boolean isWordFreeOfExcludedLetters(String word, List<String> excludedLetters){
+        String lowercaseWord = word.toLowerCase();
+        return excludedLetters.stream()
+                .noneMatch(lowercaseWord::contains);
+    }
+
+
+    private List<String> getInitialResultsFor(String inputText, boolean isUsingAnagrams){
+        var words = isUsingAnagrams ? anagramFinder.getWordsMatching(inputText)
+                : wordSearcher.searchFor(inputText);
+
+        return new ArrayList<>(words);
+    }
+
+
+    private void waitForDictionaryToLoad(){
+        try{
+            dictionaryLatch.await();
+        }catch (InterruptedException e){
+            handleException(e);
+        }
+
+    }
+
+
+    private void ifNotSearching(Runnable runnable) {
+        if(isSearchRunning.get()){
+            return;
+        }
+        isSearchRunning.set(true);
+        Runnable task = () ->{
+            runnable.run();
+            isSearchRunning.set(false);
+        };
+        executor.submit(task);
+    }
+
+    private void handleException(Exception e){
+        var message = e.getMessage();
+        log(message);
+    }
+
+
+    private String getSortedWord(String word){
+        return Arrays.stream(word.split(""))
+                .sorted()
+                .collect(Collectors.joining(""));
+    }
+
+
+    private void log(String msg){
+        System.out.println("^^^ DictionaryHelperImpl: " + msg);
+    }
+
 }
